a936ead2a78b5e38bb8df48fa53c7746c611df7d
Mike Neal <coolnetmike@gmail.com>
2016-05-21 15:15:09 -0400

    Update middleware for localhost

--- server.js
+++ server.js
@@ -4,17 +4,15 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-
 app.use(function(req, res, next) {
-
   if(req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
-
 });
+
+app.use(express.static('public'));
 
 app.listen(PORT, function() {
   console.log('Express server is up on port ' + PORT);
